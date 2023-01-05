d6f61639f0ddd36baa5ca914f5385948685de993
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-01-05 11:03:21 -0500

    Finish server file additions; fill out models directory

--- models/index.js
+++ models/index.js
@@ -1,16 +1,36 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'product_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE',
+  });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through: {
+        model: ProductTag,
+        unique: false
+    },
+    as: 'products_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: {
+        model: ProductTag,
+        unique: false
+    },
+    as: 'tags_products'
+});
 
 module.exports = {
   Product,
